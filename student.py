484af87bd4de2e2b5e182916e6ac967c2f361643
Dipti-khapekar <diptikhapekar0@gmail.com>
2026-06-12 20:55:34 +0530

    update  student.py

---
+++ student.py
@@ -0,0 +1,13 @@
+class student:
+    def__init__(self,name,age,marks):
+    self.name
+    self.age
+    self.marks
+
+    def display(self):
+        print("Name:",self.name)
+        print9"AAge:",self.age)
+        print("Marks:",self.marks)
+
+        s1 = Student("Rahul",20,85)
+        s1.display()
