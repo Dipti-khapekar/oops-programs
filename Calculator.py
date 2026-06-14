25c790ab5088ca057c79c437b270c3fe42e4d87b
Dipti-khapekar <diptikhapekar0@gmail.com>
2026-06-14 15:09:17 +0530

    create Cslculator.py and Book.py and Mobile.py

---
+++ Calculator.py
@@ -0,0 +1,19 @@
+class Calculator:
+ def add(self,a,b):
+    return a+b
+
+ def subtract(self,a,b):
+    return a-b
+
+ def multiply(self,a,b):
+    return a*b
+
+ def divide(self,a,b):
+    return a/b
+
+cal = Calculator()
+
+print(cal.add(10,5))
+print(cal.subtract(10,5))
+print(cal.multiply(10,5))
+print(cal.divide(10,5))
