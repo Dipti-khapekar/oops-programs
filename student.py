7357affca0fac693464c279a9cadc35090f27c05
Dipti-khapekar <diptikhapekar0@gmail.com>
2026-06-14 12:26:50 +0530

    update student.py

--- student.py
+++ student.py
@@ -1,13 +1,15 @@
-class student:
-    def__init__(self,name,age,marks):
-    self.name
-    self.age
-    self.marks
-
+class Student:
+    def __init__(self, name, age, marks):
+        self.name = name
+        self.age = age
+        self.marks = marks
+ 
     def display(self):
-        print("Name:",self.name)
-        print9"Age:",self.age)
-        print("Marks:",self.marks)
-
-        s1 = Student("Rahul",20,85)
-        s1.display()
+        print("Name:", self.name)
+        print("Age:", self.age)
+        print("Marks:", self.marks)
+ 
+ 
+s1 = Student("Rahul", 20, 85)
+s1.display()
+ 
