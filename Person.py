be8412b01a1871d6f46c3ea64ae0d0b4daa2c871
Dipti-khapekar <diptikhapekar0@gmail.com>
2026-06-14 15:24:30 +0530

    create Laptop.py and Porson.py and Product.py

---
+++ Person.py
@@ -0,0 +1,12 @@
+class Person:
+ def __init__(self,name,age):
+    self.name = name
+    self.age = age
+
+ def show(self):
+    print(self.name)
+    print(self.age)
+
+p = Person("Raj",25)
+
+p.show()
